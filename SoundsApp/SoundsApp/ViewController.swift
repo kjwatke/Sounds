285e8895d080f37ed89e4c40f52112ef9effc665
Kevin Watke <Kevin@Susanas-iMac.local>
2022-02-19 13:02:33 -0600

    Initial Commit

---
+++ SoundsApp/SoundsApp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SoundsApp
+//
+//  Created by Kevin Watke on 2/19/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
